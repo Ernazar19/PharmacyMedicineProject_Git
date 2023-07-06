268ef90cea66a6d1c5d97d83d99b1527ef5b94a9
ernazarasanbekov <ernazar.03@icloud.com>
2023-07-07 01:56:34 +0600

    Added Constructor getter setters and too_sting to Worker Class

--- src/peaksoft/model/Worker.java
+++ src/peaksoft/model/Worker.java
@@ -11,4 +11,61 @@ public class Worker {
     private String email;
     private Gender gender;
 
+    public Worker(Long id, String workerName, LocalDate dateOfBirth, String email, Gender gender) {
+        this.id = id;
+        this.workerName = workerName;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getWorkerName() {
+        return workerName;
+    }
+
+    public void setWorkerName(String workerName) {
+        this.workerName = workerName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker: " +
+                "id: " + id +
+                "workerName: " + workerName + "\n" +
+                "dateOfBirth: " + dateOfBirth + "\n" +
+                "email: " + email + "\n" +
+                "gender: " + gender;
+    }
 }
