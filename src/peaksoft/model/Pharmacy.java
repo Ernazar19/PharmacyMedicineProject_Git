432bcd678336a02ef5395ef82eb730b1715bde8e
ernazarasanbekov <ernazar.03@icloud.com>
2023-07-07 01:48:42 +0600

    Added Constructor getter setters and too_sting to Pharmacy Class

--- src/peaksoft/model/Pharmacy.java
+++ src/peaksoft/model/Pharmacy.java
@@ -1,7 +1,5 @@
 package peaksoft.model;
-
 import java.util.List;
-
 public class Pharmacy {
     private Long id;
     private String pharmacyName;
@@ -9,5 +7,61 @@ public class Pharmacy {
     private List<Medicine> medicines;
     private List<Worker> workers;
 
+    public Pharmacy(Long id, String pharmacyName, String address, List<Medicine> medicines, List<Worker> workers) {
+        this.id = id;
+        this.pharmacyName = pharmacyName;
+        this.address = address;
+        this.medicines = medicines;
+        this.workers = workers;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPharmacyName() {
+        return pharmacyName;
+    }
+
+    public void setPharmacyName(String pharmacyName) {
+        this.pharmacyName = pharmacyName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Medicine> getMedicines() {
+        return medicines;
+    }
+
+    public void setMedicines(List<Medicine> medicines) {
+        this.medicines = medicines;
+    }
+
+    public List<Worker> getWorkers() {
+        return workers;
+    }
+
+    public void setWorkers(List<Worker> workers) {
+        this.workers = workers;
+    }
+
+    @Override
+    public String toString() {
+        return "Pharmacy: " +
+                "id: " + id +"\n"+
+                "pharmacyName: " + pharmacyName+ "\n"+
+                "address: " + address + "\n"+
+                "medicines: " + medicines +"\n"+
+                "workers: " + workers;
+    }
 }
