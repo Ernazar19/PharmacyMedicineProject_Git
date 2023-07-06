05fe5386e25f8e04eb9ed67f1c8c127e56312e2d
ernazarasanbekov <ernazar.03@icloud.com>
2023-07-07 01:32:44 +0600

    Added fields to Pharmacy Class

--- src/peaksoft/model/Pharmacy.java
+++ src/peaksoft/model/Pharmacy.java
@@ -1,5 +1,13 @@
 package peaksoft.model;
 
+import java.util.List;
+
 public class Pharmacy {
+    private Long id;
+    private String pharmacyName;
+    private String address;
+    private List<Medicine> medicines;
+    private List<Worker> workers;
+
 
 }
