eacd9d894c32dc75dcf9a3d89cf56f736c671d5b
ernazarasanbekov <ernazar.03@icloud.com>
2023-07-07 09:23:10 +0600

    Added methods to Service Interface

--- src/peaksoft/service/Service.java
+++ src/peaksoft/service/Service.java
@@ -1,4 +1,17 @@
 package peaksoft.service;
 
+import peaksoft.model.Medicine;
+import peaksoft.model.Pharmacy;
+import peaksoft.model.Worker;
+
+import java.util.List;
+
 public interface Service {
+    List<Medicine>getAllMedicinesByPharmacy(String pharmacyName);
+    String addMedicinesToPharmacy(Long pharmacyId,Medicine medicine);
+    String addWorkerToPharmacy(Long pharmacyId, Worker worker);
+    String updateMedicinePrice(Long pharmacyId, Long medicineId, int price);
+    String deleteMedicineByName(Long pharmacyId,String medicineName);
+    String deleteWorkerByName(Long pharmacyId,String workerName);
+    Pharmacy getPharmacyByWorkerName(String workerName);
 }
