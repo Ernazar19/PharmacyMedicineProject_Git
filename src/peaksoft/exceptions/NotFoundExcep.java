670215f9f059f0727a660abd1debc435ecba7422
ernazarasanbekov <ernazar.03@icloud.com>
2023-07-07 10:03:18 +0600

    Added Exception class

---
+++ src/peaksoft/exceptions/NotFoundExcep.java
@@ -0,0 +1,10 @@
+package peaksoft.exceptions;
+
+public class NotFoundExcep extends Exception {
+    public NotFoundExcep() {
+    }
+
+    public NotFoundExcep(String message) {
+        super(message);
+    }
+}
